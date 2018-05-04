97bd56e0e338f692a3bb3a81e7c219f8f2ab4449
artem88 <artemik2010@gmail.com>
2018-05-04 12:00:10 +0300

    Created New Unity Project Added simple Scene and Cube object Added Script for Orbit Camera

---
+++ Assets/Scripts/OrbitCameraScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCameraScript : MonoBehaviour {
+
+
+    public float offset = 4.0f;	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = (Camera.main.transform.position + Camera.main.transform.forward * offset);
+	}
+}
